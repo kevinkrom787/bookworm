4ada97997f81317b67344ddc51098104a3cf6563
kevin <kevinkrom787@gmail.com>
2026-05-02 15:12:50 -0600

    migrate: update existing characters to bold illustration style

---
+++ migrations/004_bold_style.sql
@@ -0,0 +1,3 @@
+UPDATE characters
+SET style_descriptor = 'bold children''s book illustration, thick expressive ink outlines, vivid jewel-tone colors, richly detailed environments, exaggerated expressive faces, dynamic poses, sense of wonder'
+WHERE style_descriptor LIKE '%watercolor%';
